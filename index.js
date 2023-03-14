c4239282a67e85be3fdb383ad26418700c0dd61d
George Samuel <geosam2708@gmail.com>
2023-03-14 20:05:50 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+let num1 = 62;
+let num2 = 62/num1;
+const multiply = num1 * num2;
+let random = Math.ceil(Math.random());
+let num3 = 18;
+let num4 = 7;
+const mod  = num3%num4;
+const max = Math.max(20);
